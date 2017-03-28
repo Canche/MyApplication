2f2c6e22ab55778340bfc8b804b9b3ee8444f338
williamcanche <thefirstwill1@gmail.com>
2017-03-28 10:15:47 -0600

    modulo3

--- app/src/main/java/com/canche/myapplication/ItemDetailActivity.java
+++ app/src/main/java/com/canche/myapplication/ItemDetailActivity.java
@@ -108,4 +108,8 @@ public class ItemDetailActivity extends AppCompatActivity {
     //ergergdgdrgesrgegergergerg
     //regeargergergergergeqr
     //EGERHERHERHEWERWHEWRTHEWTR
+
+    //assfasfsadfsadfsadfsadf
+    ///dfdsfasraegferdg
+    //adfgaergaerg
 }
